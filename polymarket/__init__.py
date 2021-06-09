b540a1e455a3579e74116633eeb1045a3b374b7c
PolyTrader <82170965+PolyTrader@users.noreply.github.com>
2021-06-08 22:16:47 -0400

    Package now correctly includes the abi directory.

--- polymarket/__init__.py
+++ polymarket/__init__.py
@@ -1,6 +1,7 @@
 from .buy import buy
 from .redeem import redeem
 from .sell import sell
-from .utils import initialize_identity
+from .utils import initialize_identity, load_evm_abi
 
-__all__ = ['buy', 'sell', 'split', 'redeem', 'utils', 'initialize_identity']
+
+__all__ = ['abi', 'buy', 'sell', 'split', 'redeem', 'utils', 'initialize_identity', 'load_evm_abi']
