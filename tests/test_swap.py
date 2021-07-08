d40a784eb8b1d9833ba2810377b9e3eda910ec84
PolyTrader <82170965+PolyTrader@users.noreply.github.com>
2021-07-08 11:05:56 -0400

    lint fix

--- tests/test_swap.py
+++ tests/test_swap.py
@@ -1,10 +1,13 @@
-import polymarket
 import json
+
+import polymarket
+
 
 quickswap_address = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
 wmatic_address = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
 usdc_address = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
 decimals = 18
+
 
 def load_contract(w3, address, file_name):
     with open(file_name) as fp:
@@ -27,9 +30,11 @@ def main():
 
     withdrawal_amount = int(0.9 * deposit_amount)
     path = [wmatic_address, usdc_address]
-    quickswap.functions.swapExactETHForTokens(1, path, w3.eth.default_account, 1657180785).transact({"value": withdrawal_amount})
+    quickswap.functions.swapExactETHForTokens(1, path, w3.eth.default_account, 1657180785)\
+        .transact({"value": withdrawal_amount})
 
     print(usdc.functions.balanceOf(w3.eth.default_account).call() / 10 ** 6)
+
 
 if __name__ == "__main__":
     exit(main())
