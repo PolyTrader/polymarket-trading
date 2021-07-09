9ff6555ea07201d9a189006cfdee46e5f7f4a10f
PolyTrader <82170965+PolyTrader@users.noreply.github.com>
2021-07-09 11:54:11 -0400

    actual tests!

--- polymarket/__init__.py
+++ polymarket/__init__.py
@@ -1,11 +1,12 @@
 from .buy import buy
 from .markets import get_active_markets
 from .positions import get_positions, list_positions
+from .merge import merge
 from .redeem import redeem
 from .sell import sell
 from .split import split
 from .utils import initialize_identity, load_evm_abi
 
 
-__all__ = ['abi', 'gql', 'buy', 'sell', 'split', 'redeem', 'utils', 'initialize_identity', 'load_evm_abi',
+__all__ = ['abi', 'gql', 'buy', 'merge', 'sell', 'split', 'redeem', 'utils', 'initialize_identity', 'load_evm_abi',
            'get_positions', 'list_positions', 'get_active_markets']
--- tests/test_polymarket.py
+++ tests/test_polymarket.py
@@ -1,4 +1,5 @@
 import json
+import pytest
 
 import polymarket
 
@@ -8,6 +9,11 @@ wmatic_address = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
 usdc_address = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
 decimals = 18
 
+# Use a closed market to trade against since it will never go away.
+# Will the Ever Given exit the Great Bitter Lake in the Suez Canal by July 25?
+condition_id = "0x2a2245d9db4616bd30240e64b424e2184d7fb9ee222d72d92313e5fb962ef0fb"
+market_address = "0xe25fC2fAA77Cb4EC7312466fCC104EDB02596831"
+
 
 def load_contract(w3, address, file_name):
     with open(file_name) as fp:
@@ -16,7 +22,8 @@ def load_contract(w3, address, file_name):
     return w3.eth.contract(address=address, abi=abi)
 
 
-def main():
+@pytest.fixture(scope="module")
+def web3():
     w3 = polymarket.initialize_identity()
 
     quickswap = load_contract(w3, quickswap_address, 'tests/router.json')
@@ -24,17 +31,38 @@ def main():
     usdc = load_contract(w3, usdc_address, 'polymarket/abi/ERC20.json')
 
     balance = w3.eth.get_balance(w3.eth.default_account)
-    deposit_amount = int(balance/2)
+    deposit_amount = int(0.1 * balance)
     wmatic.functions.deposit().transact({"value": deposit_amount})
     wmatic.functions.approve(quickswap_address, deposit_amount).transact()
 
-    withdrawal_amount = int(0.9 * deposit_amount)
+    withdrawal_amount = int(0.5 * deposit_amount)
     path = [wmatic_address, usdc_address]
     quickswap.functions.swapExactETHForTokens(1, path, w3.eth.default_account, 1657180785)\
         .transact({"value": withdrawal_amount})
 
-    print(usdc.functions.balanceOf(w3.eth.default_account).call() / 10 ** 6)
+    return w3
 
 
-if __name__ == "__main__":
-    exit(main())
+def test_buy(web3):
+    trx = polymarket.buy(web3, market_address, 1, 0, 0.01)
+    assert trx is not None
+
+
+def test_sell(web3):
+    trx = polymarket.sell(web3, market_address, 0.1, 0, 1)
+    assert trx is not None
+
+
+def test_split(web3):
+    trx = polymarket.split(web3, condition_id, 2, 1)
+    assert trx is not None
+
+
+def test_merge(web3):
+    trx = polymarket.merge(web3, condition_id, 2, 1)
+    assert trx is not None
+
+
+def test_redeem(web3):
+    trx = polymarket.redeem(web3, condition_id, 2)
+    assert trx is not None
