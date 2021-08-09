f2e5f9aa20dec07d15cf0aa88d058645f72d28b3
PolyTrader <82170965+PolyTrader@users.noreply.github.com>
2021-08-08 21:10:24 -0400

    working code, need to clean up

--- polymarket/__init__.py
+++ polymarket/__init__.py
@@ -3,11 +3,11 @@ from .markets import get_active_markets
 from .merge import merge
 from .positions import get_positions, list_positions
 from .redeem import redeem
-from .sell import sell
+from .sell import sell, sell_shares
 from .split import split
 from .utils import initialize_identity, load_evm_abi
 from .amm import *
 
 
-__all__ = ['abi', 'gql', 'buy', 'merge', 'sell', 'split', 'redeem', 'utils', 'initialize_identity', 'load_evm_abi',
-           'get_positions', 'list_positions', 'get_active_markets']
+__all__ = ['abi', 'gql', 'buy', 'merge', 'sell', 'sell_shares', 'split', 'redeem', 'utils', 'initialize_identity',
+           'load_evm_abi', 'get_positions', 'list_positions', 'get_active_markets']
--- polymarket/sell.py
+++ polymarket/sell.py
@@ -1,5 +1,6 @@
 from .utils import conditional_token_approve_for_all, load_evm_abi
-
+from .amm import calc_sell_amount_in_collateral, get_pool_balances
+from .markets import get_active_markets
 
 def sell(web3_provider, market_maker_address, return_amount, index, maximum_shares):
     fixed_product_market_maker_address_abi = load_evm_abi('FixedProductMarketMaker.json')
@@ -10,6 +11,35 @@ def sell(web3_provider, market_maker_address, return_amount, index, maximum_shar
     conditional_token_approve_for_all(web3_provider, market_maker_address, True)
     contract = web3_provider.eth.contract(address=market_maker_address, abi=fixed_product_market_maker_address_abi)
     trx_hash = contract.functions.sell(fixed_return_amount, index, fixed_maximum_shares).transact()
+    web3_provider.eth.wait_for_transaction_receipt(trx_hash)
+
+    conditional_token_approve_for_all(web3_provider, market_maker_address, False)
+
+    return trx_hash
+
+
+def sell_shares(web3_provider, slug, outcome, num_shares, slippage):
+    market_json = get_active_markets(slug=slug)[0]
+    fixed_product_market_maker_address_abi = load_evm_abi('FixedProductMarketMaker.json')
+
+    fixed_share_count = int(num_shares * (10**6))
+    condition_id = market_json['conditionId']
+    market_maker_address = market_json['marketMakerAddress']
+    num_outcomes = len(market_json['outcomes'])
+    outcome_index = market_json['outcomes'].index(outcome)
+    fee = float(int(market_json['fee']) / (10**18))
+
+    conditional_token_approve_for_all(web3_provider, market_maker_address, True)
+    contract = web3_provider.eth.contract(address=market_maker_address, abi=fixed_product_market_maker_address_abi)
+
+    pool_balances = get_pool_balances(web3_provider, market_maker_address, condition_id, num_outcomes)
+    sell_amount_in_usdc = calc_sell_amount_in_collateral(fixed_share_count, outcome_index, pool_balances, fee)
+
+    fixed_return_amount = int(round(float(sell_amount_in_usdc)))
+
+    slippage_fixed_share_count = int(fixed_share_count * (1 + (slippage/100)))
+
+    trx_hash = contract.functions.sell(fixed_return_amount, outcome_index, slippage_fixed_share_count).transact()
     web3_provider.eth.wait_for_transaction_receipt(trx_hash)
 
     conditional_token_approve_for_all(web3_provider, market_maker_address, False)
