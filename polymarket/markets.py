7681f13298f889d11dd6ed848353494e4ea2aa98
PolyTrader <82170965+PolyTrader@users.noreply.github.com>
2022-01-29 23:23:35 -0500

    don't get closed markets

--- polymarket/markets.py
+++ polymarket/markets.py
@@ -1,10 +1,10 @@
 import requests
 
 
-def get_active_markets(slug=None):
-    api_uri = "https://strapi-matic.poly.market/markets?_limit=-1&active=true"
+def get_active_markets(slug=None, look_back=None):
+    api_uri = "https://strapi-matic.poly.market/markets?_limit=-1&active=true&closed=false"
 
     if slug is not None:
-        api_uri += f"&slug={slug}"
+        api_uri = f"https://strapi-matic.poly.market/markets?slug={slug}"
     r = requests.get(api_uri)
     return r.json()
